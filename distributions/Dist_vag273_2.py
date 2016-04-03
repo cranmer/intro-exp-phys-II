57a63a6d35fbecb31be62cdcbbd988e431a56919
vag273 <Home@172-16-158-49.DYNAPOOL.NYU.EDU>
2016-04-03 15:12:30 -0400

    dist_vag273_2
    class name change

--- distributions/Dist_vag273_2.py
+++ distributions/Dist_vag273_2.py
@@ -2,7 +2,7 @@
 import numpy as np
 from base_distribution import BaseDistribution
 
-class Dist_vag273(BaseDistribution):
+class Dist_vag273_2(BaseDistribution):
 	def __init__(self):
 		self.f_max = 2/np.sqrt(pi)
 		self.x_min = 0
