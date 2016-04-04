32e01d35eadd20be6b2b04661f33c28eaf117c69
Kyle Cranmer <kyle.cranmer@nyu.edu>
2016-04-04 10:16:19 -0400

    saving changes

--- distributions/__init__.py
+++ distributions/__init__.py
@@ -16,9 +16,13 @@ from Dist_jeg535 import Dist_jeg535
 from Dist_kc90 import Dist_kc90
 from Dist_kc90_2 import Dist_kc90_2
 from Dist_kc90_3 import Dist_kc90_3
+from Dist_mh3908 import Dist_mh3908
+from Dist_my1462 import Dist_my1462
+from Dist_ptf223 import Dist_ptf223
+from Dist_rdr335 import Dist_rdr335
 from Dist_recast import Dist_recast
 from Dist_vag273 import Dist_vag273
-from Dist_vag273_1 import Dist_vag273
+from Dist_vag273_1 import Dist_vag273_1
 from Dist_vag273_2 import Dist_vag273_2
 
 __all__ = ("Dist_ac5790",
@@ -26,7 +30,13 @@ __all__ = ("Dist_ac5790",
 	"Dist_kc90",
 	"Dist_kc90_2",
 	"Dist_kc90_3",
+	"Dist_mh3908",
+	"Dist_my1462",
+	"Dist_ptf223",
+	"Dist_rdr335",
 	"Dist_recast",
 	"Dist_vag273",
 	"Dist_vag273",
 	"Dist_vag273_2",)
+
+
