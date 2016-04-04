c8db5e8495a7584830601d95f619e2ecb6006e7a
Michael Chess <michael.chess14@trinityschoolnyc.org>
2016-04-03 21:01:20 -0400

    new pdf with x^3

--- distributions/Dist_msc565.py
+++ distributions/Dist_msc565.py
@@ -4,19 +4,19 @@ from base_distribution import BaseDistribution
 
 class Dist_msc565(BaseDistribution):
 	def __init__(self):
-		self.f_max = 2*np.log(2)
-		self.x_min = 1
-		self.x_max = 2
+		self.f_max = 2
+		self.x_min = 0
+		self.x_max = 1
 
 
 	def pdf(self, x):
 		"""This is your PDF"""
-		return np.log(x)*x
+		return (((x-0.5)**3)+0.125)*8
 
 	def mean(self):
 		"""This is the mean of the PDF"""
-		return 1.0706147037154097140
+		return 0.6
 
 	def std(self):
 		"""This is the standard deviation of the pdf"""
-		return np.sqrt(0.271988)
+		return np.sqrt(0.07333333333333333333333)
