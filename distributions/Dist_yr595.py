1054437d9ec9b341cf2b18107ece9a17f9e148b2
Eugene Reznikov <reznikov.eugene@gmail.com>
2016-04-03 22:42:57 -0400

    Added new distribution
    Added a new semicircle distribution in the correct format to the
    Distributions folder.

---
+++ distributions/Dist_yr595.py
@@ -0,0 +1,22 @@
+
+import numpy as np
+from base_distribution import BaseDistribution
+
+class Dist_yr595(BaseDistribution):
+	def __init__(self):
+		self.f_max = 1
+		self.x_min = -1
+		self.x_max = 1
+
+
+	def pdf(self, x):
+		"""This is your PDF"""
+		return 2/np.PI * np.sqrt(1-x**2)
+
+	def mean(self):
+		"""This is the mean of the PDF"""
+		return 0.
+
+	def std(self):
+		"""This is the standard deviation of the pdf"""
+		return 0.5
