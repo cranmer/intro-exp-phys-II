3d44d59833908b0bd8b3dd6051513ed4c1f216bc
Kyle Cranmer <kyle.cranmer@nyu.edu>
2016-04-04 20:15:54 -0400

    updating notebook to current distributions

--- distributions/__init__.py
+++ distributions/__init__.py
@@ -12,11 +12,14 @@ grep -h class Dist*py | sed 's/(BaseDistribution):/\",/' | sed 's/class /\"/'
 """
 
 from Dist_ac5790 import Dist_ac5790
+from Dist_ams1396 import Dist_ams1396
+from Dist_ejk483 import Dist_ejk483
 from Dist_jeg535 import Dist_jeg535
 from Dist_kc90 import Dist_kc90
 from Dist_kc90_2 import Dist_kc90_2
 from Dist_kc90_3 import Dist_kc90_3
 from Dist_mh3908 import Dist_mh3908
+from Dist_msc565 import Dist_msc565
 from Dist_my1462 import Dist_my1462
 from Dist_ptf223 import Dist_ptf223
 from Dist_rdr335 import Dist_rdr335
@@ -26,17 +29,20 @@ from Dist_vag273_1 import Dist_vag273_1
 from Dist_vag273_2 import Dist_vag273_2
 
 __all__ = ("Dist_ac5790",
+	"Dist_ams1396",
+	"Dist_ejk483",
 	"Dist_jeg535",
 	"Dist_kc90",
 	"Dist_kc90_2",
 	"Dist_kc90_3",
 	"Dist_mh3908",
+	"Dist_msc565",
 	"Dist_my1462",
 	"Dist_ptf223",
 	"Dist_rdr335",
 	"Dist_recast",
 	"Dist_vag273",
-	"Dist_vag273",
+	"Dist_vag273_1",
 	"Dist_vag273_2",)
 
 
