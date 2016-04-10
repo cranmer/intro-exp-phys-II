cc8ef33f2746b1df26da163a3502895e71fed1ea
Paige Colley <pc1727@nyu.edu>
2016-04-10 18:17:44 -0400

    altered and fixed

--- Dist_pc1727.py
+++ Dist_pc1727.py
@@ -10,7 +10,7 @@ class Dist_pc1727(BaseDistribution):
 
 	def pdf(self, x):
 		"""This is your PDF"""
-		return x^4
+		return x**4
 
 	def mean(self):
 		"""This is the mean of the PDF"""
@@ -18,5 +18,28 @@ class Dist_pc1727(BaseDistribution):
 
 	def std(self):
 		"""This is the standard deviation of the pdf"""
-		return np.sqrt(0.2857)
+		return np.sqrt((((self.x_max)**7)/7)-(((self.x_min)**7)/7))
 		
+def test(cls):
+	try:
+		dist = cls()
+		N_test = 100000
+		rvs = dist.rvs(N_test)
+		if np.abs(np.mean(rvs) - dist.mean()) > 5*np.std(rvs)/np.sqrt(N_test):
+			print("means don't match for %s: %f vs. %f" %(cls.__name__, 
+														  np.mean(rvs), dist.mean()))
+			
+		elif np.abs(np.std(rvs) - dist.std()) > 5*np.std(rvs)/np.sqrt(np.sqrt(1.*N_test)):
+			print("std devs. don't match for %s: %f vs. %f" %(cls.__name__, 
+														  np.std(rvs), dist.std()))
+		
+		elif np.sum(dist.pdf(np.linspace(dist.x_min,dist.x_max,100))<0) > 0:
+			print("pdf was negative in some places")
+
+		else:
+			print("%s passes tests, adding it" %(cls.__name__))
+	except:
+		print("%s has errors. didn't work" %(cls.__name__))
+
+if __name__ == '__main__':
+	test(Dist_pc1727)
