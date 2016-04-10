29898a490363851c126cd7cefe1d009b11828a41
rdr335 <rdr335@nyu.edu>
2016-04-10 18:20:26 -0400

    Update Dist_rdr335.py

--- distributions/Dist_rdr335.py
+++ distributions/Dist_rdr335.py
@@ -13,8 +13,8 @@ class Dist_rdr335(BaseDistribution):
 
 	def mean(self):
 		"""This is the mean of the PDF"""
-		return 0.
+		return 1.
 
 	def std(self):
 		"""This is the standard deviation of the pdf"""
-		return 0.1
+		return 1.075
