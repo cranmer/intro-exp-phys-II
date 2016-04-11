252d90c92a85cd83f95af2650425da027729cfe8
rdr335 <rdr335@nyu.edu>
2016-04-10 23:33:28 -0400

    Update Dist_rdr335.py

--- distributions/Dist_rdr335.py
+++ distributions/Dist_rdr335.py
@@ -1,20 +1,21 @@
 import numpy as np
 from base_distribution import BaseDistribution
 
+c = np.sqrt(2/np.pi)
 class Dist_rdr335(BaseDistribution):
 	def __init__(self):
-		self.f_max = np.sqrt(2/np.pi)
-		self.x_min = -np.sqrt(2/np.pi)
-		self.x_max = np.sqrt(2/np.pi)
+		self.f_max = c
+		self.x_min = -c
+		self.x_max = c
 		
 	def pdf(self, x):
 		"""This is your PDF"""
-		return np.sqrt(2/np.pi - x**2)
+		return np.sqrt(c**2 - x**2)
 
 	def mean(self):
 		"""This is the mean of the PDF"""
-		return 1.
+		return (c**3)*np.pi/8
 
 	def std(self):
 		"""This is the standard deviation of the pdf"""
-		return 1.0766
+		return (c**3)*np.pi/2
