d54e178c8b1d087a044e8d7a29f3a74bf2dbefe8
andrew502 <azaw502@gmail.com>
2016-04-11 01:01:19 -0400

    az1332 distribution

--- distributions/Dist_az1332.py
+++ distributions/Dist_az1332.py
@@ -4,21 +4,21 @@ from base_distribution import BaseDistribution
 class Dist_az1332(BaseDistribution):
 	def __init__(self):
 		self.f_max = 1
-		self.x_min = -200
-		self.x_max = 200
+		self.x_min = -10
+		self.x_max = 10
 
 
 	def pdf(self, x):
-		#"""This is your PDF""" x
-		return x**2 + np.log(x)
+		"""This is your PDF""" x
+		return x**2
 
 	def mean(self):
-		#"""This is the mean of the PDF"""
+		"""This is the mean of the PDF"""
 		return 0.
 
 	def std(self):
-		#"""This is the standard deviation of the pdf"""
-		return np.sqrt(0.5)
+		"""This is the standard deviation of the pdf"""
+		return 5.972581
 
 
 def test(cls):
