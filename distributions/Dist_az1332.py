d7891f90b9df89dde30c5d81a3a0116776d01a55
andrew502 <azaw502@gmail.com>
2016-04-14 19:22:51 -0400

    d

--- distributions/Dist_az1332.py
+++ distributions/Dist_az1332.py
@@ -14,7 +14,7 @@ class Dist_az1332(BaseDistribution):
 
 	def mean(self):
 		#"""This is the mean of the PDF"""
-		return 0
+		return 0.
 
 	def std(self):
 		#"""This is the standard deviation of the pdf"""
