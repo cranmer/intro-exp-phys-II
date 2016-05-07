74bc8b1b4a7af8f8ec30935d78f4e1e3ea05277a
purplesushi <ac5790@nyu.edu>
2016-05-07 19:12:42 -0400

    pt 2

--- distributions/Dist_psa251.py
+++ distributions/Dist_psa251.py
@@ -41,28 +41,5 @@ class Dist_psa251(BaseDistribution):
         f1 = self.f_max * ((1./3.)*self.x_max**3. - (1./5.)*self.x_max**5.) - (2.*self.mu**2.) + (self.f_max*self.mu**2.)*(self.x_max - (1./3.)*(self.x_max**3.))
 
         return np.sqrt(f1 - f0)
-
-def test(cls):
-	try:
-		dist = cls()
-		N_test = 100000
-		rvs = dist.rvs(N_test)
-		if np.abs(np.mean(rvs) - dist.mean()) > 5*np.std(rvs)/np.sqrt(N_test):
-			print("means don't match for %s: %f vs. %f" %(cls.__name__,
-														  np.mean(rvs), dist.mean()))
-
-		elif np.abs(np.std(rvs) - dist.std()) > 5*np.std(rvs)/np.sqrt(np.sqrt(1.*N_test)):
-			print("std devs. don't match for %s: %f vs. %f" %(cls.__name__,
-														  np.std(rvs), dist.std()))
-
-		elif np.sum(dist.pdf(np.linspace(dist.x_min,dist.x_max,100))<0) > 0:
-			print("pdf was negative in some places")
-
-		else:
-			print("%s passes tests, adding it" %(cls.__name__))
-	except:
-		print("%s has errors. didn't work" %(cls.__name__))
-        print traceback.print_exc()
-
 if __name__ == '__main__':
-	test(Dist_psa251)
+    test(Dist_psa251)
--- distributions/Dist_psa251_2.py
+++ distributions/Dist_psa251_2.py
@@ -34,29 +34,5 @@ class Dist_psa251_2(BaseDistribution):
         mu_sq = (1./self.lamda)**2.
 
         return np.sqrt(mu_sq) # might as well return mu, but this form is from Integral[(x - µ)^2 * p(x) * dx]
-
-
-def test(cls):
-	try:
-		dist = cls()
-		N_test = 100000
-		rvs = dist.rvs(N_test)
-		if np.abs(np.mean(rvs) - dist.mean()) > 2*np.std(rvs)/np.sqrt(N_test):
-			print("means don't match for %s: %f vs. %f" %(cls.__name__,
-														  np.mean(rvs), dist.mean()))
-
-		elif np.abs(np.std(rvs) - dist.std()) > 2*np.std(rvs)/np.sqrt(np.sqrt(1.*N_test)):
-			print("std devs. don't match for %s: %f vs. %f" %(cls.__name__,
-														  np.std(rvs), dist.std()))
-
-		elif np.sum(dist.pdf(np.linspace(dist.x_min,dist.x_max,100))<0) > 0:
-			print("pdf was negative in some places")
-
-		else:
-			print("%s passes tests, adding it" %(cls.__name__))
-	except:
-		print("%s has errors. didn't work" %(cls.__name__))
-        print traceback.print_exc()
-
 if __name__ == '__main__':
 	test(Dist_psa251_2)
