3967eb54b0ee76f3b86820f80c791cbcdb95aa8c
Paul Erlanger <paulerlanger10@gmail.com>
2018-04-22 22:42:30 -0400

    Paul's PDF function
    Here I choose the PDF x^2+3 and calculated its xmin/max, mean and std

---
+++ distributions/Dist_pme240.py
@@ -0,0 +1,22 @@
+
+import numpy as np
+from .base_distribution import BaseDistribution
+
+class Dist_(BaseDistribution):
+	def __init__(self):
+		self.f_max = 3
+		self.x_min = 0
+		self.x_max = .32936
+
+
+	def pdf(self, x):
+		"""This is your PDF"""
+		return x**2 + 3
+
+	def mean(self):
+		"""This is the mean of the PDF"""
+		return .1656589
+
+	def std(self):
+		"""This is the standard deviation of the pdf"""
+		return .040377
