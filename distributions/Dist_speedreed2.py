eb35fb3a476022dc2014d239e3371e4dee478e5f
speedreed <64391623+speedreed@users.noreply.github.com>
2020-04-27 07:08:08 -0500

    Create Dist_speedreed2.py

---
+++ distributions/Dist_speedreed2.py
@@ -0,0 +1,21 @@
+import numpy as np
+from .base_distribution import BaseDistribution
+
+class Dist_speedreed2(BaseDistribution):
+	def __init__(self):
+		self.f_max = 2
+		self.x_min = -2
+		self.x_max = 1
+
+
+	def pdf(self, x):
+		"""This is your PDF"""
+		return np.abs(x)
+
+	def mean(self):
+		"""This is the mean of the PDF"""
+		return 0.
+
+	def std(self):
+		"""This is the standard deviation of the pdf"""
+		return 2*np.sqrt(0.5)
