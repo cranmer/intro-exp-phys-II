8c88a4a7b5f738b510bff9303985c030350264c0
erinmcgrath14 <64394262+erinmcgrath14@users.noreply.github.com>
2020-04-27 08:20:03 -0600

    Create Dist_emm815.py

---
+++ distributions/Dist_emm815.py
@@ -0,0 +1,21 @@
+import numpy as np
+from .base_distribution import BaseDistribution
+
+class Dist_emm815(BaseDistribution):
+	def __init__(self):
+		self.f_max = 1
+		self.x_min = -1
+		self.x_max = 1
+
+
+	def pdf(self, x):
+		"""This is your PDF"""
+		return np.abs(x)
+
+	def mean(self):
+		"""This is the mean of the PDF"""
+		return 0.
+
+	def std(self):
+		"""This is the standard deviation of the pdf"""
+		return np.sqrt(0.5)
