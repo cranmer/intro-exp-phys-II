4480cfa43bc9dbc6e96d131c8801d7ecbac94cfa
Kyle Cranmer <kyle.cranmer@nyu.edu>
2020-04-27 09:42:48 -0500

    Update Dist_kc90.py

--- distributions/Dist_kc90.py
+++ distributions/Dist_kc90.py
@@ -3,8 +3,8 @@ from .base_distribution import BaseDistribution
 
 class Dist_kc90(BaseDistribution):
 	def __init__(self):
-		self.f_max = 1
-		self.x_min = -1
+		self.f_max = 2
+		self.x_min = -2
 		self.x_max = 1
 
 
@@ -18,4 +18,4 @@ class Dist_kc90(BaseDistribution):
 
 	def std(self):
 		"""This is the standard deviation of the pdf"""
-		return np.sqrt(0.5)
+		return 2*np.sqrt(0.5)
