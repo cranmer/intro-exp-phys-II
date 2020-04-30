e9c8e5438a4f007e896f137e084ed0fff7657856
bturkkani <bturkkani@nyu.edu>
2020-05-01 02:40:35 +0300

    Update Dist_bt1369.py

--- distributions/Dist_bt1369.py
+++ distributions/Dist_bt1369.py
@@ -3,9 +3,9 @@ from .base_distribution import BaseDistribution
 
 class Dist_bt1369(BaseDistribution):
 	def __init__(self):
-		self.f_max = 1
-		self.x_min = -1
-		self.x_max = 1
+		self.f_max = 0.22214
+		self.x_min = 0.86288
+		self.x_max = 8.44000
 
 
 	def pdf(self, x):
@@ -14,7 +14,7 @@ class Dist_bt1369(BaseDistribution):
 
 	def mean(self):
 		"""This is the mean of the PDF"""
-		return 0.
+		return 5.105308322606092.
 
 	def std(self):
 		"""This is the standard deviation of the pdf"""
