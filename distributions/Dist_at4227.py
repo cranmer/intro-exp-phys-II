1aeeb278357ba03d93e0d8bd87951274c5bb6b1f
tschinkela <64272707+tschinkela@users.noreply.github.com>
2020-04-30 21:41:40 -0500

    The pdf is a sine function.

--- distributions/Dist_at4227.py
+++ distributions/Dist_at4227.py
@@ -1,18 +1,17 @@
 import numpy as np
 from .base_distribution import BaseDistribution
-
 class Dist_at4227(BaseDistribution):
 	def __init__(self):
 		self.f_max = 1
-		self.x_min = -1
-		self.x_max = 4
+		self.x_min = 0
+		self.x_max = np.pi
 
 
 	def pdf(self, x):
-		return np.abs(x)
+		return np.sin(x)/2
 
 	def mean(self):
-		return 1.5
+		return np.pi/2
 
 	def std(self):
-		return np.sqrt(0.5)
+		return .96668517
