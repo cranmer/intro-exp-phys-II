2eab9d75e6c5b3619c48b15899f0999b44ebd09b
RashedRifat <59259308+RashedRifat@users.noreply.github.com>
2020-04-30 23:39:45 -0400

    Update Dist_rmr557.py
    Created new pdf 1/14(3x^2 - 6x + 9) and filled in the appropriate data

--- distributions/Dist_rmr557.py
+++ distributions/Dist_rmr557.py
@@ -3,19 +3,19 @@ from .base_distribution import BaseDistribution
 
 class Dist_rmr557(BaseDistribution):
 	def __init__(self):
-		self.f_max = 1
-		self.x_min = -1
-		self.x_max = 1
+		self.f_max = 9.0/14
+		self.x_min = 0
+		self.x_max = 2
 
 
 	def pdf(self, x):
 		"""This is your PDF"""
-		return np.abs(x)
+		return (1.0/14)(3.0*(x**2) - 6.0*x + 9.0)
 
 	def mean(self):
 		"""This is the mean of the PDF"""
-		return 0.
+		return 1
 
 	def std(self):
 		"""This is the standard deviation of the pdf"""
-		return np.sqrt(0.5)
+		return np.sqrt(455) / 35 
