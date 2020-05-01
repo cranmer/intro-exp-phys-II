680e677786f15815efd9eb7af7e39369f9d51009
Antony Sun <zs1427@nyu.edu>
2020-05-01 11:19:15 +0800

    Update Dist_zs1427.py

--- distributions/Dist_zs1427.py
+++ distributions/Dist_zs1427.py
@@ -3,20 +3,20 @@ from .base_distribution import BaseDistribution
 
 class Dist_zs1427(BaseDistribution):
 	def __init__(self):
-		self.f_max = 1
-		self.x_min = -1
+		self.f_max = 50
+		self.x_min = 0.02
 		self.x_max = 1
 
 
 	def pdf(self, x):
 		"""This is your PDF"""
-		return np.abs(x)
+		return (1/x)
 
 	def mean(self):
 		"""This is the mean of the PDF"""
-		return 0.
+		return 0.98 
 
 	def std(self):
 		"""This is the standard deviation of the pdf"""
-		return np.sqrt(0.5)
+		return 2.3361
 
