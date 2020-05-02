378a95fde353d4a9e6abf7df2fe49601f82aa61d
sdl433 <59803959+sdl433@users.noreply.github.com>
2020-05-02 17:26:53 -0400

    Update Dist_sdl433.py

--- distributions/Dist_sdl433.py
+++ distributions/Dist_sdl433.py
@@ -3,46 +3,18 @@ from .base_distribution import BaseDistribution
 
 class Dist_sdl433(BaseDistribution):
 	def __init__(self):
-		self.f_max = 1
-		self.x_min = -1
+		self.x_min = 0
 		self.x_max = 1
-        self.f = np.array([])
-        self.domain = np.array([])
+		self.f_max = 3
 
-    #Exponential distribution starting at x in [0,1]
-    #Takes in x as an array
+   	##U-quad distribution for domain [0, 1]
 	def pdf(self, x):
-
-        #Define the domain
-        x_min = 0
-        x_max = 1
-        domain = np.linspace(x_min, x_max, num = 10000)
-
-        #Define exponential function
-        f = 10 * np.e ** (-10 * x)
-
-        #Calculate distribution function at x
-        f_of_x = f[x * 10000]
-
-        #Integrate
-        Z = np.trapz(f, x, dx = 1/10000)
-
-        return f_of_x / Z
-
+		return 12 * ((x - 0.5) ** 2)
+	
+	#Returns mean of the pdf
 	def mean(self):
-        """This is the mean of the pdf"""
-		return np.trapz(
-            domain * pdf(domain),
-            domain,
-            dx = 1/10000
-            )
-
+        	return 0.5
+	
+	#Returns standard deviation of the pdf
 	def std(self):
-		"""This is the standard deviation of the pdf"""
-		return np.sqrt(
-            np.trapz(
-                pdf(domain) * (domain - mean) ** 2),
-                domain,
-                dx = 1/10000
-                )
-            )
+		return np.sqrt(0.15)
