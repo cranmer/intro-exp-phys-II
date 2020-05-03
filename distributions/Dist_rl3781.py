8f3df183560a9feef28a4bd5627a864f6161feb1
RuihaoLi-hub <60353685+RuihaoLi-hub@users.noreply.github.com>
2020-05-03 21:50:30 +0800

    Update Dist_rl3781.py

--- distributions/Dist_rl3781.py
+++ distributions/Dist_rl3781.py
@@ -3,21 +3,22 @@ from .base_distribution import BaseDistribution
 
 class Dist_kc90(BaseDistribution):
 	def __init__(self):
-		self.f_max = 1
-		self.x_min = -1
-		self.x_max = 1
+		self.f_max = 0.9340322474
+		self.x_min = -(1+5**(1/2))/2 
+		self.x_max = 1/137
 
 
 	def pdf(self, x):
 		"""This is your PDF"""
-		return np.abs(x)
+		return 1.1064101553*sin(2**(x))
 
-	def mean(self):
+    def mean(self):
 		"""This is the mean of the PDF"""
-		return 0.
+		return -0.6757350573
 
+	
 	def std(self):
 		"""This is the standard deviation of the pdf"""
-		return np.sqrt(0.5)
+		return 0.455796824
 
 
