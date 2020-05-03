913772f40d51f806ee9857031f1465ee27331ed5
sjamshahi <60614097+sjamshahi@users.noreply.github.com>
2020-05-02 21:14:38 -0400

    Update Dist_sj2879.py

--- distributions/Dist_sj2879.py
+++ distributions/Dist_sj2879.py
@@ -1,21 +1,21 @@
+import math as math
 import numpy as np
 from .base_distribution import BaseDistribution
 
 class Dist_sj2879(BaseDistribution):
 	def __init__(self):
-		self.f_max = 1
-		self.x_min = -1
-		self.x_max = 1
-
+		self.f_max = 36
+		self.x_min = -3
+		self.x_max = 3
 
 	def pdf(self, x):
 		"""This is your PDF"""
-		return np.abs(x)
+		return (x**2)/(18) #4x^2 normalized over range of [-3, 3] gives this pdf
 
 	def mean(self):
 		"""This is the mean of the PDF"""
-		return 0.
+		return 0
 
 	def std(self):
 		"""This is the standard deviation of the pdf"""
-		return np.sqrt(0.5)
+		return np.sqrt((27)/5)
