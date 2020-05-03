83ecc97374e110aa600ef5f5f3c2a41bca06167e
iadler19 <42978499+iadler19@users.noreply.github.com>
2020-05-03 12:59:36 -0400

    Update Dist_ia1113.py

--- distributions/Dist_ia1113.py
+++ distributions/Dist_ia1113.py
@@ -3,19 +3,19 @@ from .base_distribution import BaseDistribution
 
 class Dist_ia1113(BaseDistribution):
 	def __init__(self):
-		self.f_max = 1
-		self.x_min = -1
-		self.x_max = 1
+		self.f_max = 2.541
+		self.x_min = 7.0
+		self.x_max = 7.5
 
 
 	def pdf(self, x):
 		"""This is your PDF"""
-		return np.abs(x)
+		return np.exp(x) 
 
 	def mean(self):
 		"""This is the mean of the PDF"""
-		return 0.
+		return 7.271
 
 	def std(self):
 		"""This is the standard deviation of the pdf"""
-		return np.sqrt(0.5)
+		return np.sqrt(0.020576)
