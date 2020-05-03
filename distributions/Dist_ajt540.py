0d6ba1d9fff9e6cd32799785d379bc7f32f1fc15
andreas-tsantilas <64418562+andreas-tsantilas@users.noreply.github.com>
2020-05-03 18:56:16 -0400

    ajt540 pdf
    Polynomial pdf

--- distributions/Dist_ajt540.py
+++ distributions/Dist_ajt540.py
@@ -3,19 +3,20 @@ from .base_distribution import BaseDistribution
 
 class Dist_ajt540(BaseDistribution):
 	def __init__(self):
-		self.f_max = 1
-		self.x_min = -1
+		self.f_max = 2.19566
+		self.x_min = 0
 		self.x_max = 1
 
 
 	def pdf(self, x):
 		"""This is your PDF"""
-		return np.abs(x)
+		return (420/307.0)*(5*x**6 - 17*x**5 + 3*x**4 + 9*x**3)
 
 	def mean(self):
 		"""This is the mean of the PDF"""
-		return 0.
+		return (417.0/614.0)
 
 	def std(self):
 		"""This is the standard deviation of the pdf"""
-		return np.sqrt(0.5)
+		return np.sqrt(34003.0/1130988.0)
+		
