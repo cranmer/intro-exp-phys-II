215d1d084c28d19d2a7012f5a8aa278816c93c2b
Samuel Bekker <sb6187@nyu.edu>
2020-05-03 21:59:21 -0400

    Create Dist_sb6187.py

---
+++ distributions/Dist_sb6187.py
@@ -0,0 +1,22 @@
+import numpy as np
+import math
+from .base_distribution import BaseDistribution
+
+class Dist_sb6187(BaseDistribution):
+	def __init__(self):
+		self.f_max = 0.2
+		self.x_min = -2
+		self.x_max = 4.5
+
+
+	def pdf(self, x):
+		"""This is your PDF"""
+		return (0.0765440563)*((math.sin(x)*math.cos(x))+2)
+
+	def mean(self):
+		"""This is the mean of the PDF"""
+		return 1.344
+
+	def std(self):
+		"""This is the standard deviation of the pdf"""
+		return 1.902
