e907d0f11e53242ef365189dce0920a9b63628f0
Jonah Goldfine <60613947+JonahDG@users.noreply.github.com>
2020-05-03 21:34:24 -0700

    Update Dist_jdg577.py

--- distributions/Dist_jdg577.py
+++ distributions/Dist_jdg577.py
@@ -1,20 +1,22 @@
-m .base_distribution import BaseDistribution
-
+import numpy as np
+from .base_distribution import BaseDistribution
+from scipy.stats import norm
+import matplotlib.pyplot as plt
 class Dist_jdg577(BaseDistribution):
 	def __init__(self):
-		self.f_max = 1
-		self.x_min = -1
-		self.x_max = 1
+		self.f_max = 2/(np.e * np.sqrt(2*np.pi))
+		self.x_min = -10
+		self.x_max = 10
 
 
 	def pdf(self, x):
-		"""This is your PDF"""
-		return np.abs(x)
+		return (x ** 2) * (np.exp(-x ^ 2 / 2))/np.sqrt(2*np.pi)
+
 
 	def mean(self):
 		"""This is the mean of the PDF"""
-		return 0.
+		return 0
 
 	def std(self):
 		"""This is the standard deviation of the pdf"""
-		return np.sqrt(0.5)
+		return np.sqrt(3)
