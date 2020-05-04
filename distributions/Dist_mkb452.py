cc7c1b2b13608b0c3cda78600a241d47ab28af6c
Kaz <64378613+mkb452@users.noreply.github.com>
2020-05-04 11:54:22 -0400

    Update Dist_mkb452.py

--- distributions/Dist_mkb452.py
+++ distributions/Dist_mkb452.py
@@ -3,22 +3,22 @@ from .base_distribution import BaseDistribution
 
 class Dist_mkb452(BaseDistribution):
 	def __init__(self):
-		self.f_max = 1
-		self.x_min = -np.inf
-		self.x_max = np.inf
+		self.f_max = 0.5
+		self.x_min = 2
+		self.x_max = 5
 
 
 	def pdf(self, x):
 		"""This is your PDF"""
-		y = (-np.square(x))/2
-		return ((1/(np.sqrt(2*np.pi)))np.exp(y))
+		y = (1/0.000022737)
+		return (y*np.exp(-x,3)*np.sin(x))
 
 	def mean(self):
 		"""This is the mean of the PDF"""
-		return 0
+		return 2.06994
 
 	def std(self):
 		"""This is the standard deviation of the pdf"""
-		return 1
+		return 0.066015
 
 
