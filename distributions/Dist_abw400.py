131f2228b11d73b28d6064bef43a7cfe0f57a274
allisonbari <64416910+allisonbari@users.noreply.github.com>
2020-05-04 09:49:00 -0400

    Update Dist_abw400.py

--- distributions/Dist_abw400.py
+++ distributions/Dist_abw400.py
@@ -4,18 +4,18 @@ from .base_distribution import BaseDistribution
 class Dist_abw400(BaseDistribution):
 	def __init__(self):
 		self.f_max = 1
-		self.x_min = -1
+		self.x_min = 0
 		self.x_max = 1
 
 
 	def pdf(self, x):
 		"""This is your PDF"""
-		return np.abs(x)
+		return np.sqrt(x)/(2/3)
 
 	def mean(self):
 		"""This is the mean of the PDF"""
-		return 0.
+		return (2/5)
 
 	def std(self):
 		"""This is the standard deviation of the pdf"""
-		return np.sqrt(0.5)
+		return 0.269
