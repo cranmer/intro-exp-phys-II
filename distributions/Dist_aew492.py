922cc5df28520dc67b7879be897a639741056efd
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2020-05-04 09:51:44 -0400

    Update Dist_aew492.py

--- distributions/Dist_aew492.py
+++ distributions/Dist_aew492.py
@@ -3,7 +3,7 @@ from .base_distribution import BaseDistribution
 
 class Dist_aew492(BaseDistribution):
 	def __init__(self):
-		self.f_max = 1
+		self.f_max = 0.85
 		self.x_min = 0
 		self.x_max = 1.5
 
@@ -14,10 +14,10 @@ class Dist_aew492(BaseDistribution):
 
 	def mean(self):
 		"""This is the mean of the PDF"""
-		return 0.667
+		return 0.7174
 
 	def std(self):
 		"""This is the standard deviation of the pdf"""
-		return 0.469
+		return 0.1504
 
 
