db398606018e5ec5206a2a4ed6d84f1f3a2d769c
ElsaGrace <64579390+ElsaGrace@users.noreply.github.com>
2020-05-04 18:31:10 +0800

    Update Dist_yx1796.py

--- distributions/Dist_yx1796.py
+++ distributions/Dist_yx1796.py
@@ -4,20 +4,20 @@ from .base_distribution import BaseDistribution
 class Dist_yx1796(BaseDistribution):
 	def __init__(self):
 		self.f_max = 1
-		self.x_min = -1
-		self.x_max = 1
+		self.x_min = 2
+		self.x_max = 4
 
 
 	def pdf(self, x):
 		"""This is your PDF"""
-		return np.abs(x)
+		return (x**3)/60
 
 	def mean(self):
 		"""This is the mean of the PDF"""
-		return 0.
+		return 0.33
 
 	def std(self):
 		"""This is the standard deviation of the pdf"""
-		return np.sqrt(0.5)
+		return np.sqrt(0.266)
 
 
