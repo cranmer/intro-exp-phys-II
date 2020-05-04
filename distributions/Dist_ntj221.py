66e50825323a4c980c2faa941b1067f73bead4ff
Nojoson <37663970+Nojoson@users.noreply.github.com>
2020-05-03 20:11:23 -0400

    Update Dist_ntj221.py

--- distributions/Dist_ntj221.py
+++ distributions/Dist_ntj221.py
@@ -3,19 +3,19 @@ from .base_distribution import BaseDistribution
 
 class Dist_ntj221(BaseDistribution):
 	def __init__(self):
-		self.f_max = 1
-		self.x_min = -1
+		self.f_max = 65/120
+		self.x_min = 0
 		self.x_max = 1
 
 
 	def pdf(self, x):
 		"""This is your PDF"""
-		return np.abs(x)
+		return x**4+(7/8)*x**2
 
 	def mean(self):
 		"""This is the mean of the PDF"""
-		return 0.
+		return 185/236.
 
 	def std(self):
 		"""This is the standard deviation of the pdf"""
-		return np.sqrt(0.5)
+		return np.sqrt(12473/389872)
