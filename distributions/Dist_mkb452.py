9a10eba953470c8e75bc3409ff89835abcdd730a
Kaz <64378613+mkb452@users.noreply.github.com>
2020-05-04 22:20:28 -0400

    Update Dist_mkb452.py

--- distributions/Dist_mkb452.py
+++ distributions/Dist_mkb452.py
@@ -3,9 +3,9 @@ from .base_distribution import BaseDistribution
 
 class Dist_mkb452(BaseDistribution):
 	def __init__(self):
-		self.f_max = 0.5
+		self.f_max = 13.5
 		self.x_min = 2
-		self.x_max = 5
+		self.x_max = 2.4
 
 
 	def pdf(self, x):
@@ -15,10 +15,10 @@ class Dist_mkb452(BaseDistribution):
 
 	def mean(self):
 		"""This is the mean of the PDF"""
-		return 2.06994
+		return 2.06619
 
 	def std(self):
 		"""This is the standard deviation of the pdf"""
-		return 0.066015
+		return 0.06436
 
 
