fb56ddb8cc1735549a80e2ed4eb824244dfc1c6e
Tenzin Tsega <tsegalaa@gmail.com>
2020-05-06 18:20:59 -0400

    p

--- distributions/Dist_tt1392.py
+++ distributions/Dist_tt1392.py
@@ -10,7 +10,7 @@ class Dist_tt1392(BaseDistribution):
 
 	def pdf(self, x):
 		"""This is your PDF"""
-		return 1/pi * cos**2(x)
+		return 1/np.pi * np.cos**2(x)
 
 	def mean(self):
 		"""This is the mean of the PDF"""
