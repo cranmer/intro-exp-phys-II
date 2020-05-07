e7d42982d8b1eba41cfa48eaad18afc6af53f4bd
YongningLei <yl4874@nyu.edu>
2020-05-07 01:46:53 -0400

    my abs(lnx) pdf

--- distributions/Dist_yl4874.py
+++ distributions/Dist_yl4874.py
@@ -1,21 +1,24 @@
+#Yongning Lei (Intro to Experimental Phy Lab II) P = lnx
+
 import numpy as np
 from .base_distribution import BaseDistribution
 
-class Dist_kc90(BaseDistribution):
+
+class Dist_yl4874(BaseDistribution):
 	def __init__(self):
 		self.f_max = 1
-		self.x_min = -1
+		self.x_min = 0
 		self.x_max = 1
 
 
 	def pdf(self, x):
-		"""This is your PDF"""
-		return np.abs(x)
+
+		return np.abs(lnx) #normalizing lnx  in bounds 0,1 gives us the function abs(lnx)
 
 	def mean(self):
-		"""This is the mean of the PDF"""
-		return 0.
+
+		return (0.25)  #integral of xf(x) = integral x*abs(lnx) from 0 to 1
 
 	def std(self):
-		"""This is the standard deviation of the pdf"""
-		return np.sqrt(0.5)
+
+		return np.sqrt(7/144) #sqrt variance = sqrt ( integral (x-0.25)^2 f(x))
