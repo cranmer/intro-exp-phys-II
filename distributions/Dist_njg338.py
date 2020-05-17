7feb4aeca1cd4f7f3f1a7b83bae784667df8bb9b
njg338 <njg338@nyu.edu>
2020-05-17 15:03:47 -0400

    Update Dist_njg338.py

--- distributions/Dist_njg338.py
+++ distributions/Dist_njg338.py
@@ -4,20 +4,15 @@ from .base_distribution import BaseDistribution
 class Dist_njg338(BaseDistribution):
 	def __init__(self):
 		self.f_max = 1
-		self.x_min = -1
-		self.x_max = 1
+		self.x_min = -2*pi
+		self.x_max = 2*pi
 
 
 	def pdf(self, x):
-		"""This is your PDF"""
-		return np.abs(x)
+		return np.cos(x)
 
 	def mean(self):
-		"""This is the mean of the PDF"""
-		return 0.
+		return 0
 
 	def std(self):
-		"""This is the standard deviation of the pdf"""
-		return np.sqrt(0.5)
-
-
+		return np.sqrt(8*pi)
