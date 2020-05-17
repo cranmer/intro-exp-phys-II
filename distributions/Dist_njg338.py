29eb2364b7b0a1be6616dafe52478b3c7244b789
njg338 <njg338@nyu.edu>
2020-05-17 18:22:14 -0400

    Update Dist_njg338.py

--- distributions/Dist_njg338.py
+++ distributions/Dist_njg338.py
@@ -1,19 +1,19 @@
 import numpy as np
+import math
 from .base_distribution import BaseDistribution
 
 class Dist_njg338(BaseDistribution):
 	def __init__(self):
 		self.f_max = 1
-		self.x_min = -2*(np.pi)
-		self.x_max = 2*(np.pi)
+		self.x_min = -2*(math.pi)
+		self.x_max = 2*(math.pi)
 
 
 	def pdf(self, x):
-		return ((np.cos(x))**2)
+		return ((math.cos(x))**2)
 
 	def mean(self):
 		return 0
 
 	def std(self):
-		#return np.sqrt(((8*(np.pi)*(np.pi)*(np.pi))+(3*(np.pi)))/3)
-		return 3.7
+		return math.sqrt(((8*((math.pi)**3))+(3*(math.pi)))/3)
